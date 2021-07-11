0db8aa2300d546b1952edcb6406ce7dc940fec0a
lagishtein <lagishtein@gmail.com>
2021-07-11 17:47:44 -0400

    End of starting-point

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -2,19 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {Appointment} from '../src/Appointment'
 describe('Appointment', () => {
+    let container;
+    let customer;
+    const render = component =>  ReactDOM.render(component, container)
+    beforeEach(() => {
+        container = document.createElement('div')
+    });
     it("Renders customer's first name", () => {
-        const customer = {fistName: 'Ashley'}
-        const component = <Appointment customer={customer} />;
-        const container = document.createElement('div')
-        ReactDOM.render(component, container)
+        customer = {firstName: 'Ashley'}
+        render(<Appointment customer={customer} />)
         expect(container.textContent).toMatch('Ashley')
     });
     it("Renders another customer's first name", () => {
-        const customer = {fistName: 'Ashley'}
-        const component = <Appointment customer={customer} />;
-        const container = document.createElement('div')
-        ReactDOM.render(component, container)
+        customer = {firstName: 'Lila'}
+        render(<Appointment customer={customer} />)
         document.body.appendChild(container)
-        expect(document.body.textContent).toMatch('Jordan')
+        expect(container.textContent).toMatch('Lila')
     });
 });
